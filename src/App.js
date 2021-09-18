7a0532e0e565c7104d21903666b9c9e4acb43641
kaaatg <katrinanouvelle@gmail.com>
2021-09-18 18:47:43 +0800

    initial commit
    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. ---kaaaat
+          Edit <code>src/App.js</code> and save to reload. BLACKPINK IN YOUR AREA !
         </p>
         <a
           className="App-link"
